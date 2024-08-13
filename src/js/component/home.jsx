856be3b2627f6b2a2c3d86fd45dc4f74f0f90c5d
JhoelSand <126299517+Jhoel1999@users.noreply.github.com>
2024-08-13 22:59:18 +0000

    Primer avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [inputValue, setInputValue] = useState("");
+    const [tareas, setTareas] = useState([]);
+
+    return (
+        <div className="container mt-4">
+            <h1 className="text-center">Lista de Tareas</h1>
+            <div className="bg-light w-50 mx-auto rounded">
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <input
+                            type="text"
+							className="w-100 border-0"
+                            placeholder={tareas.length === 0 ? "No hay tareas, añadir tareas." : "¿Qué hay que hacer?"}
+                            onChange={(e) => setInputValue(e.target.value)}
+                            value={inputValue}
+                            onKeyUp={(e) => {
+                                if (e.key === "Enter" && inputValue.trim() !== "") {
+                                    setTareas(tareas.concat([inputValue.trim()]));
+                                    setInputValue("");
+                                }
+                            }}
+                        />
+                    </li>
+
+                    {tareas.map((tarea, index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between align-items-center px-4">
+                            <span>{tarea}</span>
+                            <span className="p-0">
+                                <i className="fa-solid fa-x d-none"
+                                    onClick={() => {
+                                        setTareas(tareas.filter((t, currentIndex) => index !== currentIndex));
+                                    }}
+                                ></i>
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+                <div className="text-danger p-2">{tareas.length} tareas pendientes</div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
