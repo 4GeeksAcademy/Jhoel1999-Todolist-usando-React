961be652a2a59b6b17bcbf64d9362d15dce7e8d5
JhoelSand <126299517+Jhoel1999@users.noreply.github.com>
2024-08-14 04:40:10 +0000

    Lista de tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [tareas, setTareas] = useState([]);
+    //1. Declarar el espacio del input (formulario)
+    const [tarea, setTarea] = useState("");
+    const [lista, setLista] = useState([]);
 
     return (
         <div className="container mt-4">
@@ -14,32 +15,40 @@ const Home = () => {
                         <input
                             type="text"
 							className="w-100 border-0"
-                            placeholder={tareas.length === 0 ? "No hay tareas, añadir tareas." : "¿Qué hay que hacer?"}
-                            onChange={(e) => setInputValue(e.target.value)}
-                            value={inputValue}
+                            placeholder={lista.length === 0 ? "No hay tareas, añadir tareas." : "¿Qué hay que hacer?"}
+                            /*2. Asignar el evento onChange para vigilar los cambios*/
+                            onChange={(e) =>{console.log(tarea); setTarea(e.target.value);}}
+                            value={tarea}
+                            /*3. Asignar el evento onkeyUp para agregar una tarea a la lista*/
                             onKeyUp={(e) => {
-                                if (e.key === "Enter" && inputValue.trim() !== "") {
-                                    setTareas(tareas.concat([inputValue.trim()]));
-                                    setInputValue("");
+                                if (e.key === "Enter" && tarea !== "") {
+                                    console.log("agregar");
+                                    setLista(lista.concat([tarea]));
+                                    setTarea("");
                                 }
                             }}
                         />
                     </li>
 
-                    {tareas.map((tarea, index) => (
+                    {/*4. Mostrar la tarea en la lista*/}
+                    {lista.map((tarea, index) => (
                         <li key={index} className="list-group-item d-flex justify-content-between align-items-center px-4">
                             <span>{tarea}</span>
                             <span className="p-0">
                                 <i className="fa-solid fa-x d-none"
+                                    /*5. Eliminar la lista cada tarea*/
                                     onClick={() => {
-                                        setTareas(tareas.filter((t, currentIndex) => index !== currentIndex));
+                                        console.log("eliminado");
+                                        setLista(lista.filter((t, currentIndex) => index !== currentIndex));
                                     }}
                                 ></i>
                             </span>
                         </li>
                     ))}
                 </ul>
-                <div className="text-danger p-2">{tareas.length} tareas pendientes</div>
+                <div className="text-danger p-2">
+                    {lista.length} tareas pendientes
+                </div>
             </div>
         </div>
     );
